72719efc327f0428ce2d4862a9e859a2067bc931
wleite <wleite@student.42sp.org.br>
2023-10-05 20:41:22 -0300

    feat: add function ft_write

--- header.h
+++ header.h
@@ -1,7 +1,10 @@
 #ifndef HEADER
 # define HEADER
 
+#include <sys/types.h>
+
 int ft_call(void);
 size_t ft_strlen(const char *s);
+ssize_t ft_write(int fd, const void *buf, size_t count);
 
 #endif
--- main.c
+++ main.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 #include "header.h"
+#include <unistd.h>
+#include <errno.h>
 
 int main(void) {
 	// printf("%d", ft_call());
-	char* str = strdup("Libasmr");
+	// char* str = strdup("Libasmr");
 
-	printf("%ld\n", strlen(str));
-	printf("%ld\n", ft_strlen(str));
+	// printf("%ld\n", strlen(str));
+	// printf("%ld\n", ft_strlen(str));
 
-	printf("%s\n", str);
+	// printf("%s\n", str);
 
-	free(str);
+	// free(str);
+
+	// ssize_t ret = ft_write(1, "zé da manga\n", 14);
+	// printf("%ld", ret);
+
+	// printf("%ld", write(90, "hello ", 7));
+	// printf("\n%s", strerror(errno));
+	printf("%ld", ft_write(90, "hello ", 7));
+	printf("\n%s", strerror(errno));
 
 	return (0);
 }
