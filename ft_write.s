0997ce0c5a0b71aab8b7c283e4b718f2975116ae
wleite <wleite@student.42sp.org.br>
2023-10-12 01:46:03 -0300

    feat: set errno in ft_write

--- ft_write.s
+++ ft_write.s
@@ -1,7 +1,19 @@
+extern __errno_location
+
 section .text
     global ft_write
 
 ft_write:
     mov rax, 1
     syscall
+    cmp rax, -1
+    js .error
+    ret
+
+.error:
+    neg rax
+    mov r11, rax
+    call __errno_location wrt ..plt
+    mov [rax], r11
+    mov rax, -1
     ret
