502ea6b2136528e14cafc2bfbace483dc2582b04
wleite <wleite@student.42sp.org.br>
2023-10-12 20:28:07 -0300

    feat: add ft_read declaration

--- main.c
+++ main.c
@@ -6,6 +6,8 @@
 
 int main(void) {
 	ssize_t ret = 0;
+	char buf[10];
+	buf[10] = '\0';
 	setbuf(stdout, NULL);
 	// printf("%d", ft_call());
 	// char* str = strdup("Libasmr");
@@ -22,21 +24,35 @@ int main(void) {
 
 	// printf("%ld", write(90, "hello ", 7));
 	// printf("\n%s", strerror(errno));
-	ret = ft_write(1, "hello ", 0);
+	// ret = ft_write(1, "hello ", 0);
+	// printf("%ld", ret);
+	// printf("\n%s", strerror(errno));
+
+	// printf("\n=========\n");
+
+	// ret = ft_write(90, "hello ", 7);
+	// printf("%ld", ret);
+	// printf("\n%s", strerror(errno));
+
+	// printf("\n=========\n");
+
+	// ret = ft_write(1, "hello ", 7);
+	// printf("%ld", ret);
+	// printf("\n%s", strerror(errno));
+
+	ret = ft_read(1, buf, 9);
 	printf("%ld", ret);
+	printf("%s", buf);
 	printf("\n%s", strerror(errno));
 
 	printf("\n=========\n");
 
-	ret = ft_write(90, "hello ", 7);
+	ret = ft_read(-1, buf, 9);
 	printf("%ld", ret);
+	printf("%s", buf);
 	printf("\n%s", strerror(errno));
 
 	printf("\n=========\n");
-
-	ret = ft_write(1, "hello ", 7);
-	printf("%ld", ret);
-	printf("\n%s", strerror(errno));
 
 	return (0);
 }
