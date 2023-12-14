b5b0c07058e7e7f20e62e38b6a2c9fba7ee43dd2
wleite <wleite@student.42sp.org.br>
2023-12-14 20:04:38 -0300

    test: add ft_strdup test

---
+++ test/ft_strdup.c
@@ -0,0 +1,31 @@
+#include <string.h>
+
+#include "../src/header.h"
+#include "ctest.h"
+
+CTEST(ft_strdup, test1) {
+  char * or = strdup("");
+  char *ft = ft_strdup("");
+  ASSERT_STR(or, ft);
+
+  free(or);
+  free(ft);
+}
+
+CTEST(ft_strdup, test2) {
+  char * or = strdup("42");
+  char *ft = ft_strdup("42");
+  ASSERT_STR(or, ft);
+
+  free(or);
+  free(ft);
+}
+
+CTEST(ft_strdup, test3) {
+  char * or = strdup("Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor!");
+  char *ft = ft_strdup("Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor!");
+  ASSERT_STR(or, ft);
+
+  free(or);
+  free(ft);
+}
