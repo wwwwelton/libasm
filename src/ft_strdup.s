b089caee4d67b18124e1ebb0d7d67825554e9f7b
wleite <wleite@student.42sp.org.br>
2023-12-22 23:09:11 -0300

    fix: ft_strdup invalid read

--- src/ft_strdup.s
+++ src/ft_strdup.s
@@ -4,7 +4,7 @@ section .text
 
 ;char *strdup(const char *s);
 ft_strdup:
-    mov r10, rdi    ;copy s address to tmp register
+    mov r15, rdi    ;copy s address to tmp register
     call ft_strlen  ;count s len
     add rax, 1      ;add +1 for '\0'
     mov rdi, rax    ;save s len to first param
@@ -12,8 +12,9 @@ ft_strdup:
     cmp rax, 0      ;if malloc failed == NULL go to .end
     je .end
     mov rdi, rax    ;if malloc success put address in first param (dest)
-    mov rsi, r10    ;put s address in second param (src)
+    mov rsi, r15    ;put s address in second param (src)
     call ft_strcpy  ;copy s to dest
 
 .end:
+
     ret             ;return the content in the rax
