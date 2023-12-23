da457889ce038563110224a320244f9ba912f6d1
wleite <wleite@student.42sp.org.br>
2023-12-22 22:21:40 -0300

    fix: ft_strcmp invalid read

--- src/ft_strcmp.s
+++ src/ft_strcmp.s
@@ -3,32 +3,48 @@ section .text
 
 ;int ft_strcmp(const char *s1, const char *s2);
 ft_strcmp:
-    mov r10, [rdi]  ;dereference s1 and copy to tmp register
-    mov r11, [rsi]  ;dereference s2 and copy to tmp register
+    push r10
+    push r11
+    push rdi
+    push rsi
+
+    dec rdi         ;s1--
+    dec rsi         ;s2--
 
 .loop:
-    cmp r10b, r11b  ;compare if *s1 == *s2
-    je .chars_equal ;if equal jump to .chars_equal
-    jl .less
-    jmp .above
+    inc rdi         ;s1++
+    inc rsi         ;s2++
+    cmp byte [rdi], 0 ;if *s1 == '\0'
+    je .check_value
+    cmp byte [rsi], 0 ;if *s2 == '\0'
+    je .check_value
+    mov al, [rdi]   ;al = *s1
+    cmp al, [rsi]   ;if *s1 == *s2
+    je .loop
 
-.chars_equal:
-    cmp r10b, 0     ;compare if *s1 == '\0'
-    je .equal       ;if *s1 == '\0' jump to .equal
-    inc rdi         ;increment s1 address
-    inc rsi         ;increment s2 address
-    mov r10, [rdi]  ;dereference s1 and copy to tmp register
-    mov r11, [rsi]  ;dereference s2 and copy to tmp register
-    jmp .loop       ;jump to .loop
+.check_value:
+    mov al, [rdi]   ;al = *s1
+    sub al, [rsi]   ;*s1 - *s2
+    je .equal       ;if *s1 == *s2
+    jl .less        ;if *s1 < *s2
+    jmp .above      ;if *s1 > *s2
 
 .less:
     mov rax, -1     ;set rax (return register) to -1 (a < b)
-    ret             ;return the content in the rax
+    jmp .end
 
 .above:
-    mov rax, 1      ;set rax (return register) to 1 (b > a)
-    ret             ;return the content in the rax
+    mov rax, 1      ;set rax (return register) to  1 (a > b)
+    jmp .end
 
 .equal:
     mov rax, 0      ;set rax (return register) to 0 (a == a)
-    ret             ;return the content in the rax
+    jmp .end
+
+.end:
+    pop rsi
+    pop rdi
+    pop r11
+    pop r10
+
+    ret
