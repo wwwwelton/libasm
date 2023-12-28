f5f661d614077922c349b92f703e3bc7a55162d3
wleite <wleite@student.42sp.org.br>
2023-12-28 16:50:16 -0300

    refactor: add backup of registers

--- src/ft_list_size.s
+++ src/ft_list_size.s
@@ -3,15 +3,19 @@ section .text
 
 ;int ft_list_size(t_list *begin_list);
 ft_list_size:
+    push r15
+
     mov rax, 0      ;int size = 0;
-    mov r11, rdi    ;node = begin_list;
+    mov r15, rdi    ;node = begin_list;
 
 .loop:
-    cmp r11, 0      ;check if node is null
+    cmp r15, 0      ;check if node is null
     je .end         ;return the rax(size)
     inc rax         ;size++
-    mov r11, [r11 + 8] ;node = node->next
+    mov r15, [r15 + 8] ;node = node->next
     jmp .loop       ;back to loop (while...)
 
 .end:
+    pop r15
+
     ret             ;return the content in the rax
